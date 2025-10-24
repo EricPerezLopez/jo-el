9a8958006ef88dd9ecd79c34caa7905b25928dd0
Silvr420 <eric.perez@gracia.lasalle.cat>
2025-10-24 10:11:57 +0200

    dias de la semana

--- idk.java
+++ idk.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class idk {
-    public static void main(String[] args) {
-
-
-       float nota1=10;
-       float nota2=5;
-       float nota3=1;
-
-        
+    public static void main(String[] args) {        
         Scanner ns = new Scanner(System.in);
     
        // Nota1 = ns.nextLine();
-        float[] notas= {nota1,nota2,nota3};
-        System.out.println(notas[0]);
-        String[] noms = new String[5];
-        noms[3] = "q";
-        System.out.println(noms[3]);
-        for (int i=0; i<noms.length; i++){System.out.println(noms[i]);}
-        
+      
+        String[] dies = new String[7];
+        dies[0]="lunes";
+        dies[1]="martes";
+        dies[2]="miercoles";
+        dies[3]="jueves";
+        dies[4]="viernes";
+        dies[5]="sabado";
+        dies[6]="domingo";
+
+       // System.out.println(noms[3]);
+        //for (int i=0; i<noms.length; i++){System.out.println(noms[i]);}
+        for (int i=0; i<5;i++){
+            System.out.println("a currar " + dies[i]);
+        }
+        for (int i=5;
+        i<dies.length;
+        i++)
+        {System.out.println("descansar " + dies[i]);}
         }
   }
 
