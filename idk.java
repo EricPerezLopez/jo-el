5a15aa4ba94d473c46e37f8fc349acc2a9e9f323
Silvr420 <eric.perez@gracia.lasalle.cat>
2025-10-29 12:40:32 +0100

    vots unfinished

--- idk.java
+++ idk.java
@@ -1,33 +1,18 @@
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class idk {
     public static void main(String[] args) {        
         Scanner ns = new Scanner(System.in);
-    
-       // Nota1 = ns.nextLine();
-      
-        String[] dies = new String[7];
-        dies[0]="lunes";
-        dies[1]="martes";
-        dies[2]="miercoles";
-        dies[3]="jueves";
-        dies[4]="viernes";
-        dies[5]="sabado";
-        dies[6]="domingo";
-
-       // System.out.println(noms[3]);
-        //for (int i=0; i<noms.length; i++){System.out.println(noms[i]);}
-        for (int i=0; i<5;i++){
-            System.out.println("a currar " + dies[i]);
+        int lel;
+        lel = ns.nextInt();
+        int[] vots = new int[lel];
+        for (int i=0; i<vots.length;i++ ){
+            vots[i] = ns.nextInt();
         }
-        for (int i=5;
-        i<dies.length;
-        i++)
-        {System.out.println("descansar " + dies[i]);}
-        }
-  }
+        System.out.println(mostFreqEle(vots)); 
+       
+  }}
 
 
