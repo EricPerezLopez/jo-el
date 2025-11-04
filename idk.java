f12ca09cbc79668201dd74d4f1e9dc12f9d5acf2
Silvr420 <eric.perez@gracia.lasalle.cat>
2025-11-04 09:27:27 +0100

    pesadilla

--- idk.java
+++ idk.java
@@ -5,14 +5,34 @@ import java.util.Scanner;
 public class idk {
     public static void main(String[] args) {        
         Scanner ns = new Scanner(System.in);
+        int num;
+       num = ns.nextInt();
+        while (num>0) {
         int lel;
         lel = ns.nextInt();
-        int[] vots = new int[lel];
-        for (int i=0; i<vots.length;i++ ){
-            vots[i] = ns.nextInt();
-        }
-        System.out.println(mostFreqEle(vots)); 
-       
-  }}
+         int lol;
+        lol = ns.nextInt();
+        float[][] mem = new float[lel][lol];
+        float max=0;
+  //      for (int i=0; i<mem.length;i++){
+//int score = ns.nextInt();
+//mem[i] = score;
+       for (int i=0 ; i<mem.length;i++) {
+        max=mem[i][0];
+   for (int j=0 ; j<mem[i].length ; j++) {
+     float score = ns.nextFloat();
+mem[i][j] = score;
+ 
+ 
+ if(max<mem[i][j]){
+            max= mem[i][j];
+                }
+    }
+  }
+System.out.println(max);
+  
+}
+}
+}
 
 
